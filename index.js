34145ed3d981d55a06fd917bf2705a9add83debc
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-11 20:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase();
 }
-function ('logShout(string)', const spy = expect.spyOn(console, 'log').andCallThrough()
-logShout('hello')
-expect(spy).toHaveBeenCalledWith('HELLO')
-console.log.restore()
-})
