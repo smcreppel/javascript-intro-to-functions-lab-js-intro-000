74acf22f1c66b918703acfb6da4112ae75cf4cc0
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-11 19:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase();
 }
-'logShout(string) {
-function: console.log .toUpperCase()
-};
+'logShout(string)';{
+function console.log .toUpperCase()
+}
