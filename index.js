1a089ca923db224170d45886830a63a21078e7a7
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-11 23:08:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   return ("I can't hear you!");
 }
-EX
+function sayHiToGrandma(string){
+expect ('HELLO').toEqual('YES INDEED!');
 }
 function sayHiToGrandma (string){
   if ('I love you, Grandma.')
