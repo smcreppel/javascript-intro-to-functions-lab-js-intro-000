6780f740378bd5d1a674d0d923427e488dbb7340
SARA Creppel <saramichellecreppel@gmail.com>
2021-02-11 22:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function whisper(string){
   return string.toLowerCase();
 }
 function logShout(string) {
-  console.log ('hello');
-  toUpperCase;
+  console.log ('hello') ;toUpperCase;
 }
 
 function sayHiToGrandma (string){
